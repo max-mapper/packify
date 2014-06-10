b079770ff5e28af7ae9b95f2989800ebf3e31b79
Max Ogden <max@maxogden.com>
2014-06-09 19:57:41 -0700

    1.1.0 - add inline-images

--- cli.js
+++ cli.js
@@ -3,9 +3,14 @@
 var fs = require('fs')
 var packer = require('./')
 
+var args = process.argv.slice(2)
+var file = args.slice(0, 1)[0]
+
 var opts = {
-  html: fs.readFileSync(process.argv[2])
+  html: fs.readFileSync(file)
 }
+
+opts.args = args.slice(1)
 
 packer(opts, function(err, packed) {
   console.log(packed.toString())
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var scriptify = require('scriptify')
 var styleInliner = require('inline-styles')
+var imageInliner = require('inline-images')
 var concat = require('concat-stream')
 var path = require('path')
 var fs = require('fs')
 var url = require('url')
 
 module.exports = function(opts, cb) {
-  opts.selector = opts.selector || 'script'
-  opts.args = ['-t', 'brfs']
-  var scriptifier = scriptify(opts)
+  // inline <img> tags
+  var html = imageInliner(opts.html)
   
+  // inline <script> tags
+  opts.selector = opts.selector || 'script'
+  var scriptifier = scriptify(opts)
   scriptifier.pipe(concat(inlineStyles))
   scriptifier.on('error', cb)
-  scriptifier.write(opts.html)
+  scriptifier.write(html)
   scriptifier.end()
   
   function inlineStyles(html) {
+    // inline <link rel="stylesheet"> tags
     var inlined = styleInliner(html)
     cb(null, inlined)
   }
